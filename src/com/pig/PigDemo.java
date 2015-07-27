f83bf2ed8567a0f246430d5652250fcdb4e0b978
GURDIT_SINGH <gurdit.singh.044@gmail.com>
2015-07-27 23:45:26 +0530

    Initial commit

---
+++ src/com/pig/PigDemo.java
@@ -0,0 +1,23 @@
+package com.pig;
+
+import java.io.IOException;
+
+import org.apache.pig.PigServer;
+
+public class PigDemo {
+
+	
+	public static void main(String[] args) {
+		try {
+		    PigServer pigServer = new PigServer("local");
+		    runIdQuery(pigServer, "test.txt");
+		    }
+		    catch(Exception e) {
+		    }
+		 }
+		public static void runIdQuery(PigServer pigServer, String inputFile) throws IOException {
+		    pigServer.registerQuery("A = load '" + inputFile + "' using PigStorage(',');");
+		    pigServer.registerQuery("B = foreach A generate $0 as id;");
+		    pigServer.store("B", "out");
+		 }
+}
